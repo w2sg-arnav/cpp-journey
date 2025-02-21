beb1ffd4e2966d1265f78749eb5ab33ae1a011c5
w2sg-arnav <sonavane.arnav2@gmail.com>
2025-02-21 11:22:36 +0530

    cpp journey

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "number 1" << std::endl;
+    std::cout << "number 2" << std::endl;
+    std::cout << "number 3" << std::endl;
+
+    return 0;
+}
